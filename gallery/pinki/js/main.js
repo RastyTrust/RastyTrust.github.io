7e33a189456d79ed4578d936a7e7ef1d2dedb665
RT <fiasko666@gmail.com>
2025-05-19 21:31:11 +0300

    re doo pinki

--- gallery/pinki/js/main.js
+++ gallery/pinki/js/main.js
@@ -1,148 +1,117 @@
-function showForm(formName) {
-    const loginForm = document.getElementById('login-form');
-    const registerForm = document.getElementById('register-form');
-    const loginBtn = document.getElementById('login-btn');
-    const registerBtn = document.getElementById('register-btn');
+const switcherBtn = document.getElementById("switcher");
+const frontPage = document.querySelector(".front__page");
+const backPage = document.querySelector(".back__page");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const toggleBtn = document.getElementById("togglePassword");
+const eyeOpen = document.getElementById("eyeOpen");
+const eyeClosed = document.getElementById("eyeClosed");
 
-    if (formName === 'login') {
-        loginForm.classList.add('active');
-        registerForm.classList.remove('active');
-        loginBtn.classList.remove('active');
-        registerBtn.classList.add('active');
-    } else if (formName === 'register') {
-        registerForm.classList.add('active');
-        loginForm.classList.remove('active');
-        registerBtn.classList.remove('active');
-        loginBtn.classList.add('active');
-    }
+// тогл на око
+toggleBtn.addEventListener("click", () => {
+  const isHidden = passwordInput.type === "password";
+  passwordInput.type = isHidden ? "text" : "password";
+
+  eyeOpen.style.display = isHidden ? "none" : "block";
+  eyeClosed.style.display = isHidden ? "block" : "none";
+});
+
+
+// відключаємо кнопку до заповнення полів
+function toggleSwitcherState() {
+  const isEmailValid = emailInput.value.trim() !== "";
+  const isPasswordValid = passwordInput.value.trim().length >= 8;
+
+  switcherBtn.disabled = !(isEmailValid && isPasswordValid);
 }
 
-/* відразу показує форму логіна */
-showForm('login');
+// лісенери
+emailInput.addEventListener("input", toggleSwitcherState);
+passwordInput.addEventListener("input", toggleSwitcherState);
 
-$(function () {
-    const container = document.querySelector('.why-join-container');
-    const slides = document.querySelectorAll('.why-join-section');
-
-    let currentIndex = 0;
-
-    function autoScroll() {
-        if (window.innerWidth <= 780) {
-            currentIndex = (currentIndex + 1) % slides.length;
-            container.scrollTo({
-                left: slides[currentIndex].offsetLeft,
-                behavior: 'smooth',
-            });
-        }
-    }
-
-    setInterval(autoScroll, 4000);
-
-    $('.scroll-to').click(function () {
-        var target = $(this).data('target');
-        if ($(target).length) {
-            $('html, body').animate(
-                {
-                    scrollTop: $(target).offset().top,
-                },
-                500,
-            );
-        }
-    });
-
-    
-    $('.pngs-carousel').slick({
-        adaptiveHeight: true,
-        arrows: false,
-        dots: false,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        centerMode: false,
-        variableWidth: false,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        infinite: true,
-        pauseOnHover: true,
-        swipeToSlide: true,
-        focusOnSelect: true,
-        responsive: [
-            {
-                breakpoint: 1480,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 780,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-        ],
-    });
-
-    $('.gifs-carousel').slick({
-        adaptiveHeight: true,
-        arrows: false,
-        dots: false,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        centerMode: false,
-        variableWidth: false,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        infinite: true,
-        pauseOnHover: true,
-        swipeToSlide: true,
-        focusOnSelect: true,
-        responsive: [
-            {
-                breakpoint: 1480,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 780,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-        ],
-    });
-
-    const background = document.querySelector('.background');
-    for (let i = 0; i < 20; i++) {
-        const circle = document.createElement('div');
-        circle.className = 'circle';
-
-        const size = Math.floor(Math.random() * 150) + 50;
-        const top = Math.random() * 100;
-        const left = Math.random() * 100;
-
-        circle.style.width = `${size}px`;
-        circle.style.height = `${size}px`;
-        circle.style.top = `${top}vh`;
-        circle.style.left = `${left}vw`;
-
-        background.appendChild(circle);
-    }
+// світчер
+switcherBtn.addEventListener("click", () => {
+  frontPage.style.display = "none";
+  backPage.style.display = "block";
 });
+
+// неактивна кнопка
+switcherBtn.disabled = true;
+
+ $('.slick-photo').slick({
+        adaptiveHeight: true,
+        arrows: false,
+        dots: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        centerMode: false,
+        variableWidth: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        pauseOnHover: true,
+        swipeToSlide: true,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 1480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 780,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+
+    $('.slick-vid').slick({
+        adaptiveHeight: true,
+        arrows: false,
+        dots: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        centerMode: false,
+        variableWidth: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        pauseOnHover: true,
+        swipeToSlide: true,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 1480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 780,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
